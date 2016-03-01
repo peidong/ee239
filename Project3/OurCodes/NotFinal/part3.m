d9acea8221914f7bc6e3d59a704e41f0d737b29b
peidong <peidong@outlook.com>
2016-03-01 14:45:45 -0800

    update

--- Project3/OurCodes/NotFinal/part3.m
+++ Project3/OurCodes/NotFinal/part3.m
@@ -31,27 +31,27 @@ Rmat_thresholded_2(find(Wmat == 0)) = -1;
 k = [10,50,100];
 
 for itr=1:length(k)
-    
+
     for cross_validate = 1:10
-        
+
         Rmat_part2 = Rmat;
         Rmat_part1 = Rmat;
-        
+
         for st = steps(cross_validate):steps(cross_validate)+10000-1
             ind = index(st);
             Rmat_part2(u(ind,1),u(ind,2)) = nan;
             Rmat_part1(u(ind,1),u(ind,2)) = nan;
-        end        
-        
+        end
+
         [U_1,V_1] = wnmfrule(Rmat_part1,k(itr),option);
         UV_1 = U_1*V_1;
-        
+
         th = 1;
         for thresh = 0.2:0.2:4.8
             UV_1_thresholded = UV_1;
             UV_1_thresholded(find(UV_1 <= thresh)) = -1;
             UV_1_thresholded(find(UV_1 > thresh)) = 1;
-            
+
             gt = [];
             dt = [];
             for st = steps(cross_validate):steps(cross_validate)+10000-1
@@ -61,20 +61,20 @@ for itr=1:length(k)
                 gt = [gt Rmat_thresholded(i,j)];
                 dt = [dt UV_1_thresholded(i,j)];
             end
-            
+
             temp1 = dt-gt;
             temp2 = dt+gt;
             tp = length(find(temp2 == 2));
             tn = length(find(temp2 == -2));
             fp = length(find(temp1 == 2));
             fn = length(find(temp1 == -2));
-            
+
             pr_part1(cross_validate,th,itr) = tp/(tp+fp);
             rec_part1(cross_validate,th,itr) = tp/(tp+fn);
             th = th+1;
-                      
+
         end
-        
+
     end
 end
 
@@ -82,4 +82,4 @@ mean_pr = mean(pr_part1,1);
 mean_rec = mean(rec_part1,1);
 mean_pr_2 = mean(pr_part2,1);
 mean_rec_2 = mean(rec_part2,1);
-save('part4_full.mat');
+save('part3_full.mat');
